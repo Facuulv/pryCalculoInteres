8fba4b8be516c2cd41a21e51df93b5fbbd263d64
Facundo Lopez Vital <facu_lv_13@hotmail.com>
2024-10-26 18:58:08 -0300

    Agregar archivos de proyecto.

---
+++ pryCalculoInteres/Form1.Designer.cs
@@ -0,0 +1,190 @@
+﻿namespace pryCalculoInteres
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnCalcular = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtMonto = new System.Windows.Forms.TextBox();
+            this.numMeses = new System.Windows.Forms.NumericUpDown();
+            this.numInteres = new System.Windows.Forms.NumericUpDown();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.lblValorCuota = new System.Windows.Forms.Label();
+            this.txtTotal = new System.Windows.Forms.TextBox();
+            this.lblError = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.numMeses)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numInteres)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.Location = new System.Drawing.Point(147, 197);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(100, 41);
+            this.btnCalcular.TabIndex = 0;
+            this.btnCalcular.Text = "Calcular";
+            this.btnCalcular.UseVisualStyleBackColor = true;
+            this.btnCalcular.Click += new System.EventHandler(this.btnCalcular_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(144, 81);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(99, 16);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Ingresar monto:";
+            // 
+            // txtMonto
+            // 
+            this.txtMonto.Location = new System.Drawing.Point(147, 110);
+            this.txtMonto.Name = "txtMonto";
+            this.txtMonto.Size = new System.Drawing.Size(100, 22);
+            this.txtMonto.TabIndex = 2;
+            // 
+            // numMeses
+            // 
+            this.numMeses.Location = new System.Drawing.Point(31, 157);
+            this.numMeses.Name = "numMeses";
+            this.numMeses.Size = new System.Drawing.Size(120, 22);
+            this.numMeses.TabIndex = 3;
+            // 
+            // numInteres
+            // 
+            this.numInteres.Location = new System.Drawing.Point(244, 157);
+            this.numInteres.Name = "numInteres";
+            this.numInteres.Size = new System.Drawing.Size(120, 22);
+            this.numInteres.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(279, 138);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(47, 16);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Interes";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(60, 138);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(48, 16);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Meses";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(60, 257);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(131, 16);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Valor de cada cuota:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(85, 299);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(41, 16);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Total:";
+            // 
+            // lblValorCuota
+            // 
+            this.lblValorCuota.AutoSize = true;
+            this.lblValorCuota.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblValorCuota.Location = new System.Drawing.Point(206, 257);
+            this.lblValorCuota.Name = "lblValorCuota";
+            this.lblValorCuota.Size = new System.Drawing.Size(2, 18);
+            this.lblValorCuota.TabIndex = 9;
+            // 
+            // txtTotal
+            // 
+            this.txtTotal.Enabled = false;
+            this.txtTotal.Location = new System.Drawing.Point(142, 296);
+            this.txtTotal.Name = "txtTotal";
+            this.txtTotal.Size = new System.Drawing.Size(100, 22);
+            this.txtTotal.TabIndex = 10;
+            // 
+            // lblError
+            // 
+            this.lblError.AutoSize = true;
+            this.lblError.Location = new System.Drawing.Point(63, 343);
+            this.lblError.Name = "lblError";
+            this.lblError.Size = new System.Drawing.Size(0, 16);
+            this.lblError.TabIndex = 11;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(418, 395);
+            this.Controls.Add(this.lblError);
+            this.Controls.Add(this.txtTotal);
+            this.Controls.Add(this.lblValorCuota);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.numInteres);
+            this.Controls.Add(this.numMeses);
+            this.Controls.Add(this.txtMonto);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnCalcular);
+            this.Name = "Form1";
+            this.Text = "Calculador de Interes";
+            ((System.ComponentModel.ISupportInitialize)(this.numMeses)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numInteres)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCalcular;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtMonto;
+        private System.Windows.Forms.NumericUpDown numMeses;
+        private System.Windows.Forms.NumericUpDown numInteres;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label lblValorCuota;
+        private System.Windows.Forms.TextBox txtTotal;
+        private System.Windows.Forms.Label lblError;
+    }
+}
+
---
+++ pryCalculoInteres/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace pryCalculoInteres
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalcular_Click(object sender, EventArgs e)
+        {
+            decimal meses = (int)numMeses.Value;
+
+            decimal interes = (int)numInteres.Value;
+
+            decimal monto = int.Parse(txtMonto.Text);
+            
+            if (meses > 0 && interes > 0 && monto > 0) {
+                decimal R = 0;
+                if (meses <= 3) {
+                    R = monto;
+                }
+                else { 
+                    R = ((monto * interes) / 100) + monto;
+                }
+                lblValorCuota.Text = "$" + (R / meses).ToString("F2");
+                txtTotal.Text = "$" + (R + monto).ToString("F2");
+                lblError.Text = "";
+            } else {
+                lblError.Text = "Error, se deben colocar valores mayores a 0";
+            }
+        }
+    }
+}
